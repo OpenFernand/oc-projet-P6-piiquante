3408859aae19d9fde16fbc4ad4871c70053c4dfa
Fernand Adjahossou <kafe@wanadoo.fr>
2023-04-17 23:45:27 +0200

    ok

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uniqueValidator = require('mongoose-unique-validator'); // package vérification d'un email unique
+//const uniqueValidator = require('mongoose-unique-validator'); // package vérification d'un email unique
 
 const userSchema = mongoose.Schema({ // Schema du modèle user exigé
     email: { type: String, required: true, unique: true }, // Unique -> une adresse mail = un user
@@ -9,6 +9,6 @@ const userSchema = mongoose.Schema({ // Schema du modèle user exigé
 
 console.log('Dans models userSchema new : ' , userSchema)
 
-userSchema.plugin(uniqueValidator); // utilisation du package. Il semble qu'il y ait un probleme de version !!!
+//userSchema.plugin(uniqueValidator); // utilisation du package. Il semble qu'il y ait un probleme de version !!!
 
 module.exports = mongoose.model('User', userSchema); // Classe 'User' dans ../models/User
