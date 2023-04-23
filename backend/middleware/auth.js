be7e11bd3999bfefaaa202ae91287740148f0295
Fernand Adjahossou <kafe@wanadoo.fr>
2023-04-23 19:56:48 +0200

    fin

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,15 +5,11 @@ module.exports = (req, res, next) => {
        const token = req.headers.authorization.split(' ')[1]; // Récupération Token
        const decodedToken = jwt.verify(token, `${process.env.JWT_KEY_TOKEN}`); // Vérification token
        const userId = decodedToken.userId; 
-
        req.auth = {
            userId: userId
        }
-       
        console.log('Le userId :' , userId)   
-
        next();
-
    } catch(error) {
        res.status(401).json({ error: new Error('Requête non authentifiée !') });
    }
